a5302a236510fbe7ca62d6931c34bf019c548003
Hamid Ali <164846674+hamidali3936@users.noreply.github.com>
2025-06-01 02:47:09 -0700

    Add files via upload
    The website is a building construction Company.

---
+++ hello.js
@@ -0,0 +1,25 @@
+function Home(){
+    document.writeln("<h1>Please coming soon!</h1>");
+}
+function Services(){
+    alert("Please coming soon!");
+}
+function Bloge(){
+    alert("Please coming soon!");
+}
+function AboutUs(){
+    alert("Please coming soon!");
+}
+function ContectMe(){
+    alert("Please coming soon!");
+}
+function loop(){
+
+let a = 1;
+while(a <= 2){
+    document.writeln("Please!<br>");
+    a = a +1;
+
+}
+}
+
